37705ee514af56170ccda8cbde2cb19a00d00ba8
yusuf <cloudengineer.yusuf@gmail.com>
2022-05-31 19:31:31 +0300

    31.05.2022 updated

--- linux/projects/Project-101/First_Part/script.sh
+++ linux/projects/Project-101/First_Part/script.sh
@@ -1,1 +1,6 @@
-cat event_history.csv | grep serdar | grep -i Terminateinstance | grep -Eo "i-[a-zA-Z0-9]{17}" > /tmp/result.txt
+# first solution
+cat event_history.csv | grep serdar | grep -i Terminateinstance | grep -Eo "i-[a-zA-Z0-9]{17}" | sort | uniq -c > /tmp/result.txt
+
+#second solution
+
+cat event_history.csv | grep serdar | grep -i Terminateinstance | grep -Eo "i-.{17}" | sort | uniq -c > /tmp/result.txt
