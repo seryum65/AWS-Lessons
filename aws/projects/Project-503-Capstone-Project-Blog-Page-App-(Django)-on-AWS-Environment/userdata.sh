97298b4568f198639ad8058d65b9cd0b4468977b
yusuf <cloudengineer.yusuf@gmail.com>
2022-07-01 21:25:34 +0300

    01.07.2022 updated

--- aws/projects/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
+++ aws/projects/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
@@ -4,13 +4,14 @@ apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
 TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+git clone https://$TOKEN@github.com/seryum65/seryum_aws_capstone_project.git
+cd /home/ubuntu/seryum_aws_capstone_project
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/seryum_aws_capstone_project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:80
+
